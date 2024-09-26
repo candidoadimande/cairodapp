17a182eff91f06425233ec9ab7e12fd7f1777fe1
candidoadimande <candidoadimande@gmail.com>
2024-09-26 09:07:01 +0200

    master

--- cairodapp/src/components/ClassComp.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-class ClassComp extends React.Component {
-    render() {
-        return <p>This is a class Component</p>
-    }
-}
-export default ClassComp
--- cairodapp/src/components/Functionalcomp.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function FunctionalComp() {
-    return <p>this is a functional component</p>
-}
-
-export default FunctionalComp
